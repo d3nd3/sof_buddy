f31628ef50ffb8184a1c38c92a059d8715717213
Graham Fenner <d3nd3second@gmail.com>
2023-11-12 02:20:19 +0000

    float help

--- src/features/media_timers.cpp
+++ src/features/media_timers.cpp
@@ -1,5 +1,8 @@
 //for _controlfp
 #include <float.h>
+#define _PC_24          0x00020000
+#define _MCW_PC         0x00030000              /* Precision Control */
+
 #include <iostream>
 
 #include "sof_compat.h"
